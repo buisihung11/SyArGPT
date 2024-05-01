3482fa4a20d4b7ff0f4979871edf809e51a279c6
HungHcc <buisihung321@gmail.com>
2024-05-01 16:48:26 +0700

    Add History, Explain section and DrawIoEmbed

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Bird,
   Book,
@@ -16,6 +18,7 @@ import {
   Triangle,
   Turtle,
 } from "lucide-react";
+import { DrawIoEmbed, DrawIoEmbedRef } from "react-drawio";
 
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
@@ -44,8 +47,26 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { ModeToggle } from "@/components/ui/mode-toggle";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { useEffect, useRef, useState } from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function Dashboard() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const drawioRef = useRef<DrawIoEmbedRef>(null);
+
+  useEffect(() => {
+    if (drawioRef.current && isLoaded) {
+      drawioRef.current.layout({
+        layouts: [],
+      });
+    }
+  }, [isLoaded]);
+
   return (
     <TooltipProvider>
       <div className="grid h-screen w-full pl-[56px]">
@@ -133,6 +154,7 @@ export default function Dashboard() {
             </Tooltip>
           </nav>
           <nav className="mt-auto grid gap-1 p-2">
+            <ModeToggle />
             <Tooltip>
               <TooltipTrigger asChild>
                 <Button
@@ -148,6 +170,7 @@ export default function Dashboard() {
                 Help
               </TooltipContent>
             </Tooltip>
+
             <Tooltip>
               <TooltipTrigger asChild>
                 <Button
@@ -288,8 +311,6 @@ export default function Dashboard() {
               </DrawerContent>
             </Drawer>
             <div className="ml-auto gap-1.5 text-sm">
-              <ModeToggle />
-
               <Button
                 variant="outline"
                 size="sm"
@@ -300,164 +321,89 @@ export default function Dashboard() {
               </Button>
             </div>
           </header>
-          <main className="grid flex-1 gap-4 overflow-auto p-4 md:grid-cols-2 lg:grid-cols-3">
-            <div
-              className="relative hidden flex-col items-start gap-8 md:flex"
-              x-chunk="dashboard-03-chunk-0"
+          <main className="flex-1 overflow-auto">
+            <ResizablePanelGroup
+              autoSaveId="persitentResize"
+              direction="horizontal"
             >
-              <form className="grid w-full items-start gap-6">
-                <fieldset className="grid gap-6 rounded-lg border p-4">
-                  <legend className="-ml-1 px-1 text-sm font-medium">
-                    Settings
-                  </legend>
-                  <div className="grid gap-3">
-                    <Label htmlFor="model">Model</Label>
-                    <Select>
-                      <SelectTrigger
-                        id="model"
-                        className="items-start [&_[data-description]]:hidden"
-                      >
-                        <SelectValue placeholder="Select a model" />
-                      </SelectTrigger>
-                      <SelectContent>
-                        <SelectItem value="genesis">
-                          <div className="flex items-start gap-3 text-muted-foreground">
-                            <Rabbit className="size-5" />
-                            <div className="grid gap-0.5">
-                              <p>
-                                Neural{" "}
-                                <span className="font-medium text-foreground">
-                                  Genesis
-                                </span>
-                              </p>
-                              <p className="text-xs" data-description>
-                                Our fastest model for general use cases.
-                              </p>
-                            </div>
-                          </div>
-                        </SelectItem>
-                        <SelectItem value="explorer">
-                          <div className="flex items-start gap-3 text-muted-foreground">
-                            <Bird className="size-5" />
-                            <div className="grid gap-0.5">
-                              <p>
-                                Neural{" "}
-                                <span className="font-medium text-foreground">
-                                  Explorer
-                                </span>
-                              </p>
-                              <p className="text-xs" data-description>
-                                Performance and speed for efficiency.
-                              </p>
-                            </div>
-                          </div>
-                        </SelectItem>
-                        <SelectItem value="quantum">
-                          <div className="flex items-start gap-3 text-muted-foreground">
-                            <Turtle className="size-5" />
-                            <div className="grid gap-0.5">
-                              <p>
-                                Neural{" "}
-                                <span className="font-medium text-foreground">
-                                  Quantum
-                                </span>
-                              </p>
-                              <p className="text-xs" data-description>
-                                The most powerful model for complex
-                                computations.
-                              </p>
-                            </div>
-                          </div>
-                        </SelectItem>
-                      </SelectContent>
-                    </Select>
-                  </div>
-                  <div className="grid gap-3">
-                    <Label htmlFor="temperature">Temperature</Label>
-                    <Input id="temperature" type="number" placeholder="0.4" />
-                  </div>
-                  <div className="grid grid-cols-2 gap-4">
-                    <div className="grid gap-3">
-                      <Label htmlFor="top-p">Top P</Label>
-                      <Input id="top-p" type="number" placeholder="0.7" />
-                    </div>
-                    <div className="grid gap-3">
-                      <Label htmlFor="top-k">Top K</Label>
-                      <Input id="top-k" type="number" placeholder="0.0" />
-                    </div>
-                  </div>
-                </fieldset>
-                <fieldset className="grid gap-6 rounded-lg border p-4">
-                  <legend className="-ml-1 px-1 text-sm font-medium">
-                    Messages
-                  </legend>
-                  <div className="grid gap-3">
-                    <Label htmlFor="role">Role</Label>
-                    <Select defaultValue="system">
-                      <SelectTrigger>
-                        <SelectValue placeholder="Select a role" />
-                      </SelectTrigger>
-                      <SelectContent>
-                        <SelectItem value="system">System</SelectItem>
-                        <SelectItem value="user">User</SelectItem>
-                        <SelectItem value="assistant">Assistant</SelectItem>
-                      </SelectContent>
-                    </Select>
-                  </div>
-                  <div className="grid gap-3">
-                    <Label htmlFor="content">Content</Label>
-                    <Textarea
-                      id="content"
-                      placeholder="You are a..."
-                      className="min-h-[9.5rem]"
-                    />
-                  </div>
-                </fieldset>
-              </form>
-            </div>
-            <div className="relative flex h-full min-h-[50vh] flex-col rounded-xl bg-muted/50 p-4 lg:col-span-2">
-              <Badge variant="outline" className="absolute right-3 top-3">
-                Output
-              </Badge>
-              <div className="flex-1" />
-              <form
-                className="relative overflow-hidden rounded-lg border bg-background focus-within:ring-1 focus-within:ring-ring"
-                x-chunk="dashboard-03-chunk-1"
-              >
-                <Label htmlFor="message" className="sr-only">
-                  Message
-                </Label>
-                <Textarea
-                  id="message"
-                  placeholder="Type your message here..."
-                  className="min-h-12 resize-none border-0 p-3 shadow-none focus-visible:ring-0"
-                />
-                <div className="flex items-center p-3 pt-0">
-                  <Tooltip>
-                    <TooltipTrigger asChild>
-                      <Button variant="ghost" size="icon">
-                        <Paperclip className="size-4" />
-                        <span className="sr-only">Attach file</span>
-                      </Button>
-                    </TooltipTrigger>
-                    <TooltipContent side="top">Attach File</TooltipContent>
-                  </Tooltip>
-                  <Tooltip>
-                    <TooltipTrigger asChild>
-                      <Button variant="ghost" size="icon">
-                        <Mic className="size-4" />
-                        <span className="sr-only">Use Microphone</span>
-                      </Button>
-                    </TooltipTrigger>
-                    <TooltipContent side="top">Use Microphone</TooltipContent>
-                  </Tooltip>
-                  <Button type="submit" size="sm" className="ml-auto gap-1.5">
-                    Send Message
-                    <CornerDownLeft className="size-3.5" />
-                  </Button>
+              <ResizablePanel defaultValue={20}>
+                <div className="relative flex h-full flex-col p-4">
+                  <h1 className="text-xl font-semibold">History</h1>
                 </div>
-              </form>
-            </div>
+              </ResizablePanel>
+              <ResizableHandle withHandle />
+              <ResizablePanel defaultSize={60}>
+                <div className="relative flex h-full min-h-[50vh] flex-col bg-muted/50">
+                  <DrawIoEmbed
+                    ref={drawioRef}
+                    onLoad={() => {
+                      setIsLoaded(true);
+                    }}
+                  />
+                  <div className="flex-1" />
+                  <form
+                    className="relative overflow-hidden rounded-lg border bg-background focus-within:ring-1 focus-within:ring-ring"
+                    x-chunk="dashboard-03-chunk-1"
+                  >
+                    <Label htmlFor="message" className="sr-only">
+                      Message
+                    </Label>
+                    <Textarea
+                      id="message"
+                      placeholder="Type your message here..."
+                      className="min-h-12 resize-none border-0 p-3 shadow-none focus-visible:ring-0"
+                    />
+                    <div className="flex items-center p-3 pt-0">
+                      <Tooltip>
+                        <TooltipTrigger asChild>
+                          <Button variant="ghost" size="icon">
+                            <Paperclip className="size-4" />
+                            <span className="sr-only">Attach file</span>
+                          </Button>
+                        </TooltipTrigger>
+                        <TooltipContent side="top">Attach File</TooltipContent>
+                      </Tooltip>
+                      <Tooltip>
+                        <TooltipTrigger asChild>
+                          <Button variant="ghost" size="icon">
+                            <Mic className="size-4" />
+                            <span className="sr-only">Use Microphone</span>
+                          </Button>
+                        </TooltipTrigger>
+                        <TooltipContent side="top">
+                          Use Microphone
+                        </TooltipContent>
+                      </Tooltip>
+                      <Button
+                        type="submit"
+                        size="sm"
+                        className="ml-auto gap-1.5"
+                      >
+                        Send Message
+                        <CornerDownLeft className="size-3.5" />
+                      </Button>
+                    </div>
+                  </form>
+                </div>
+              </ResizablePanel>
+              <ResizableHandle withHandle />
+              <ResizablePanel defaultValue={20}>
+                <div className="relative flex h-full flex-col p-4">
+                  <Tabs defaultValue="explain">
+                    <TabsList className="grid w-full grid-cols-2">
+                      <TabsTrigger value="explain">Explain</TabsTrigger>
+                      <TabsTrigger value="password">Code</TabsTrigger>
+                    </TabsList>
+                    <TabsContent value="explain">
+                      Explaintation about Diagram
+                    </TabsContent>
+                    <TabsContent value="password">
+                      Get the sample code
+                    </TabsContent>
+                  </Tabs>
+                </div>
+              </ResizablePanel>
+            </ResizablePanelGroup>
           </main>
         </div>
       </div>
