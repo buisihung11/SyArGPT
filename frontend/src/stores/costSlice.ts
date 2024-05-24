06471b172f8689c40bd1b7822cf09ab8beb77bb6
AndyTang <tangthienan9@gmail.com>
2024-05-24 22:23:51 +0700

    update slice

--- frontend/src/stores/costSlice.ts
+++ frontend/src/stores/costSlice.ts
@@ -7,9 +7,15 @@ export type CostResult = {
 }
 
 export type AppSlice = {
+  explainResult?: string
+  codeResult?: string
   costResult?: CostResult
+  imageResult?: URL
   isAppLoading: boolean
+  setExplainResult: (explainResult: string) => void
+  setCodeResult: (codeResult: string) => void
   setCostResult: (costResult: CostResult) => void
+  setImageResult: (imageResult: URL) => void
   setIsAppLoading: (isAppLoading: boolean) => void
 }
 
@@ -17,10 +23,21 @@ export const createAppSlice: StateCreator<AppSlice, [], [], AppSlice> = (
   set,
   get
 ) => ({
+  explainResult: undefined,
+  codeResult: undefined,
   costResult: undefined,
   isAppLoading: false,
+  setExplainResult: (explainResult: string) => {
+    set(state => ({ explainResult }))
+  },
+  setCodeResult: (codeResult: string) => {
+    set(state => ({ codeResult }))
+  },
   setCostResult: (costResult: CostResult) => {
     set(state => ({ costResult }))
+  },
+  setImageResult: (imageResult: URL) => {
+    set(state => ({ imageResult }))
   },
   setIsAppLoading: (isAppLoading: boolean) => {
     set(state => ({ isAppLoading }))
