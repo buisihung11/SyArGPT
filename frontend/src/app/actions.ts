1a9c138f94e863561ed2d825c5c7b6feaa289e87
HungHcc <buisihung321@gmail.com>
2024-05-25 13:29:34 +0700

    chore: Update costEstimate function in actions.ts to include specific detail in the configuration summary

--- frontend/src/app/actions.ts
+++ frontend/src/app/actions.ts
@@ -1,7 +1,5 @@
 "use server"
 
-import { z } from "zod"
-
 import { BedrockChat } from "@langchain/community/chat_models/bedrock"
 import { PromptTemplate } from "@langchain/core/prompts"
 import { StructuredOutputParser } from "@langchain/core/output_parsers"
@@ -28,10 +26,10 @@ export async function costEstimate({ input }: { input: string }) {
       As a cloud architect, you have been tasked with providing a cost estimate from a cloud architecture 
       using the code syntax from https://diagrams.mingrammer.com/. Please ensure the following:
 
-        1. Include all components from the diagram in the cost estimate.
+        1. Make sure you include all components from the diagram in the cost estimate and the number of instances.
         2. Use valid AWS component names for all services.
-        3. Please use default configurations for each service. Please give detail on the configuration summary.
-        4. The cost estimate table should have the following columns: Service Name, Monthly Cost, Region, and Config Summary.
+        3. Please use default configurations for each service. Please give specific detail on the configuration summary.
+        4. The cost estimate table should have the following columns: Service Name, Monthly Cost, Region, and Config Summary and $ currency.
         5. Provide a summary of the total cost estimate.
         Here is the cloud architecture diagram:
 
@@ -47,12 +45,11 @@ export async function costEstimate({ input }: { input: string }) {
     const model = new BedrockChat({
       model: "anthropic.claude-3-sonnet-20240229-v1:0", // You can also do e.g. "anthropic.claude-v2"
       region: "ap-southeast-2",
-      // endpointUrl: "custom.amazonaws.com",
       credentials: {
         accessKeyId: process.env.BEDROCK_AWS_ACCESS_KEY_ID!,
         secretAccessKey: process.env.BEDROCK_AWS_SECRET_ACCESS_KEY!
       },
-      modelKwargs: { temperature: 0.5 }
+      modelKwargs: { temperature: 0.1 }
     })
 
     const parser = StructuredOutputParser.fromZodSchema(CostEstimationAISchema)
@@ -80,7 +77,8 @@ export async function generateTerraformCode(body: { diagramCode: string }) {
         2. Use valid AWS component names for all services.
         3. Should have connection between components when there is a connection in the diagram.
         4. The terraform code should be in a valid format.
-        5. You can have multiple files in the terraform code if needed.
+        5. You can have multiple files in the terraform code if needed, some fundamential files should have is 
+        variables.tf, data.tf.
         6. The terraform code should be able to be run on AWS.
         7. The terraform code should follow terraform bestpractice.
         Here is the cloud architecture diagram:
