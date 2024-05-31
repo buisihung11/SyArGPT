0ee9ae085c2110959c0085f1df6446eb6fe58ec6
AndyTang <tangthienan9@gmail.com>
2024-05-31 09:52:55 +0700

    fix runtime vpbankchallenge ui error

--- frontend/src/components/ResultSection/ResultSection.tsx
+++ frontend/src/components/ResultSection/ResultSection.tsx
@@ -3,18 +3,17 @@
 import { Button } from "@/components/ui/button"
 import { AppSlice, useAppStore, useChatStore } from "@/stores"
 import { File, RefreshCcw, TriangleAlert } from "lucide-react"
+import dynamic from "next/dynamic"
 import Image from "next/image"
 import { useRouter } from "next/navigation"
-import { FC, useEffect, useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { DrawIoEmbedRef, EventExport } from "react-drawio"
 import { GridBackgroundDemo } from "../GridBackground/GridBackground"
 import HistorySection from "../HistorySection/HistorySection"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger
-} from "../ui/dropdown-menu"
+
+const NoSSRVPBankChallenges = dynamic(() => import("./VPBankChallenges"), {
+  ssr: false
+})
 
 const sampleData = [
   {
@@ -248,23 +247,7 @@ const ResultSection = () => {
                   {data.name}
                 </Button>
               ))}
-              <DropdownMenu>
-                <DropdownMenuTrigger>
-                  <VPBankButton />
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="w-56">
-                  {vpBankChallenges.map((data, idx) => (
-                    <DropdownMenuItem
-                      key={data.name}
-                      onClick={() => {
-                        setPrompt(data.prompt)
-                      }}
-                    >
-                      {data.name}
-                    </DropdownMenuItem>
-                  ))}
-                </DropdownMenuContent>
-              </DropdownMenu>
+              <NoSSRVPBankChallenges vpBankChallenges={vpBankChallenges} />
             </div>
           </div>
         </div>
@@ -346,35 +329,6 @@ function ErrorResult() {
         </div>
       </div>
     </div>
-  )
-}
-
-const VPBankButton: FC<any> = () => {
-  return (
-    <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-sm p-px text-xs font-semibold leading-6  text-white inline-block">
-      <span className="absolute inset-0 overflow-hidden rounded-sm">
-        <span className="absolute inset-0 rounded-sm bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
-      </span>
-      <div className="relative flex space-x-2 items-center z-10 rounded-sm bg-zinc-950 py-1 px-4 ring-1 ring-white/10 ">
-        <span>VPBank Challenge</span>
-        <svg
-          fill="none"
-          height="16"
-          viewBox="0 0 24 24"
-          width="16"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M10.75 8.75L14.25 12L10.75 15.25"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="1.5"
-          />
-        </svg>
-      </div>
-      <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
-    </button>
   )
 }
 
