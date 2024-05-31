9c0af1f26f333e575a8fc360317afefa1ab90895
Hung Bui <tx8@topdanmark.dk>
2024-05-31 13:05:19 +0700

    fix: cann't use button inside Dropdowntrigger

--- frontend/src/components/ResultSection/VPBankChallenges/VPBankButton.tsx
+++ frontend/src/components/ResultSection/VPBankChallenges/VPBankButton.tsx
@@ -1,6 +1,6 @@
 export const VPBankButton = () => {
   return (
-    <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-sm p-px text-xs font-semibold leading-6  text-white inline-block">
+    <div className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-sm p-px text-xs font-semibold leading-6  text-white inline-block">
       <span className="absolute inset-0 overflow-hidden rounded-sm">
         <span className="absolute inset-0 rounded-sm bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
       </span>
@@ -23,6 +23,6 @@ export const VPBankButton = () => {
         </svg>
       </div>
       <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
-    </button>
+    </div>
   )
 }
