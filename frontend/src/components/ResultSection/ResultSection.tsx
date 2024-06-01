b1c1a4e632e96cfbeb7a935e743396080f2aefff
AndyTang <tangthienan9@gmail.com>
2024-06-01 10:19:24 +0700

    feat: add gridbackground

--- frontend/src/components/ResultSection/ResultSection.tsx
+++ frontend/src/components/ResultSection/ResultSection.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { GridBackgroundDemo } from "../GridBackground/GridBackground"
 import HistorySection from "../HistorySection"
 import ImageResult from "./ImageResult"
 
@@ -7,6 +8,7 @@ const ResultSection = () => {
   return (
     <div className="relative flex flex-col h-full min-h-[50vh]">
       <div className="h-full">
+        <GridBackgroundDemo />
         <ImageResult />
       </div>
       <div className="h-48 min-h-36 justify-self-end bottom-0 w-full">
