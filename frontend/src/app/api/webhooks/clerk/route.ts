6338f020ccbf9913d59e8f3624f3d0fc67583aac
HungHcc <buisihung321@gmail.com>
2024-07-15 21:35:16 +0700

    feat: Update webhook secret variable name

--- frontend/src/app/api/webhooks/clerk/route.ts
+++ frontend/src/app/api/webhooks/clerk/route.ts
@@ -5,7 +5,13 @@ import prisma from "@/lib/prisma"
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
+  const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET
+
+  console.log(
+    "WEBHOOK_SECRET",
+    WEBHOOK_SECRET,
+    process.env.CLERK_WEBHOOK_SECRET
+  )
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
